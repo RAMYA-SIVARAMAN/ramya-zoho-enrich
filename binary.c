7168d4fc40e388d804991282abf27fb956869bdc
RAMYA-SIVARAMAN <35227671+RAMYA-SIVARAMAN@users.noreply.github.com>
2018-02-26 17:54:19 +0530

    Update binary.c

--- binary.c
+++ binary.c
@@ -1,27 +1,14 @@
 #include<stdio.h>
-
 void main()
-
-{
-    
-int n,rem,c=0;
-   
-scanf("%d",&n);
-   
-while(n>0)
-   
-{
-        
-rem=n%2;
-       
-if(rem==1)
-       
-c++;
-       
-n=n/2;
-   
- }
-  
+{    
+int n,rem,c=0;   
+scanf("%d",&n);   
+while(n>0)   
+{        
+           rem=n%2;       
+           if(rem==1)       
+           c++;       
+           n=n/2;   
+ }  
 printf("%d",c);
-
 }
